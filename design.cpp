3d17f96b7ca20c8994f1d1f4a45a83b2df800e1b
souravdebnath109 <souravdebnath54474@gmail.com>
2024-01-25 15:37:09 +0600

    final commit

--- design.cpp
+++ design.cpp
@@ -1,26 +1,26 @@
-//#include<bits/stdc++.h>
+#include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
 
 class shape{
     
 public:
- virtual void draw()=0;
+    virtual void draw()=0;
 };
 
 class rectangle : public shape{
-    public:
+public:
     void draw()
     {
         cout<<"Inside Rectangle"<<endl;
     }
 };
 
-class square : public shape{
+class circle : public shape{
 public:
     void draw()
     {
-        cout<<"Inside square"<<endl;
+        cout<<"Inside circle"<<endl;
     }
 };
 
@@ -28,44 +28,50 @@ class shapefactory{
     static shapefactory *ins;
     shapefactory() {}
 public:
+    // Eager Initialization
     static shapefactory* getinstance()
     {
-        if(ins==nullptr)
-        {
-            ins=new shapefactory();
-        }
         return ins;
+    }
+
+    static void initialize()
+    {
+        if(ins == nullptr)
+        {
+            ins = new shapefactory();
+        }
     }
 
     shape* getshape(string shapetype)
     {
-
-      if(shapetype=="rectangle")
-      {
-          return new rectangle();
-      }
-      else if(shapetype=="circle")
-      {
-          return new ();
-      }
-      else return nullptr;
+        if(shapetype=="rectangle")
+        {
+            return new rectangle();
+        }
+        else if(shapetype=="circle")
+        {
+            return new circle();
+        }
+        else return nullptr;
     }
 };
 
-shapefactory* shapefactory::ins=nullptr;
+// Eager Initialization
+shapefactory* shapefactory::ins = new shapefactory();
 
 int main()
 {
+    // Eager Initialization
+    shapefactory::initialize();
+
     shapefactory* shapefactory1 = shapefactory::getinstance();
     shapefactory* shapefactory2 = shapefactory::getinstance();
     cout<<"Address of shapefactory 1 : "<<shapefactory1<<endl;
     cout<<"Address of shapefactory 2 : "<<shapefactory2<<endl;
 
-
     shape *shape1 = shapefactory1->getshape("rectangle");
     shape1->draw();
 
-    shape *shape2 = shapefactory1->getshape("square");
+    shape *shape2 = shapefactory1->getshape("circle");
     shape2->draw();
-
 }
